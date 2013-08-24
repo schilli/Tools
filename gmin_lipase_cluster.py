7f2ef051c75d7a8c197dc61296a20bce3fbcb1c2
Oliver Schillinger <o.rusche@fz-juelich.de>
2013-08-24 16:21:52 +0200

    gmin_lipase_cluster.py can now plot collective coordinates

--- gmin_lipase_cluster.py
+++ gmin_lipase_cluster.py
@@ -12,6 +12,7 @@
 from gmin_lipase_cluster import *
 import sys, os, time, math
 import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     main()  
@@ -40,6 +41,8 @@ def main():
         structure.comp_collective_coordinates()
     runtime = time.time() - starttime
     print "{:.2f} seconds".format(runtime)
+
+    plot_CitA_collective_coordinates(structures)
 
 #    for structure in structures[:4]:
 #        print structure
@@ -150,6 +153,73 @@ def read_all_structures(directories):
 
 # ============================================================================ #
 
+def plot_CitA_collective_coordinates(structures):
+    """The function name says it"""
+
+    # Compile coordinates into array
+    energy     = np.zeros(len(structures), dtype=np.float) 
+    CitA_r     = np.zeros(len(structures), dtype=np.float)
+    CitA_theta = np.zeros(len(structures), dtype=np.float)
+    CitA_phi   = np.zeros(len(structures), dtype=np.float)
+    Lip_r      = np.zeros(len(structures), dtype=np.float)
+    Lip_theta  = np.zeros(len(structures), dtype=np.float)
+    Lip_phi    = np.zeros(len(structures), dtype=np.float) 
+
+    for i, structure in enumerate(structures):
+        energy    [i] = structure.energy
+        CitA_r    [i] = structure.CitA_r    
+        CitA_theta[i] = structure.CitA_theta
+        CitA_phi  [i] = structure.CitA_phi  
+        Lip_r     [i] = structure.Lip_r     
+        Lip_theta [i] = structure.Lip_theta 
+        Lip_phi   [i] = structure.Lip_phi   
+
+    # scale energies to 0-1 range for coloring
+    # (0 = high energy / bad, 1 = low energy / good
+    energy_colors = -1*(((energy - min(energy)) / max(energy - min(energy))) - 1)
+
+    # Create plot
+    fig = plt.figure()
+    ax  = fig.add_subplot(111)
+
+    # Plot data
+
+    what = 'CitA phi r'
+
+    if what == 'CitA phi':
+        ax.plot(CitA_phi, energy, 'o')
+        ax.set_xlabel('CitA phi (rad)')
+        ax.set_ylabel('energy (a.u.)') 
+
+    elif what == 'CitA theta':
+        ax.plot(CitA_theta, energy, 'o')
+        ax.set_xlabel('CitA theta (rad)')
+        ax.set_ylabel('energy (a.u.)')  
+
+    elif what == 'CitA r':
+        ax.plot(CitA_r, energy, 'o')
+        ax.set_xlabel('CitA r (a.u.)')
+        ax.set_ylabel('energy (a.u.)')   
+
+    elif what == 'CitA phi theta':
+        ax.plot(CitA_phi, CitA_theta, 'o')
+        ax.set_xlabel('CitA phi (a.u.)')
+        ax.set_ylabel('CitA theta (a.u.)')
+
+    elif what == 'CitA phi r':
+        ax = fig.add_subplot(111, polar=True)
+        s  = plt.scatter(CitA_phi, CitA_theta, c=energy_colors, s=10+100*energy_colors, cmap=plt.cm.cool)
+        s.set_alpha(0.75)
+        plt.colorbar()
+
+    else:
+        print "Plot option not known: {}".format(what)
+        sys.exit(1)
+
+    plt.show()
+
+# ============================================================================ #
+
 
 class Structure:
     """Contains information about a protein structure"""
@@ -173,6 +243,18 @@ class Structure:
 
         # 3*N array
         self.coordinates = [] 
+
+        # Rotation matrices
+        self.CitA_R = np.eye(3)
+        self.Lip_R  = np.eye(3)
+
+        # Collective coordinates
+        self.CitA_r     = 0.0
+        self.CitA_theta = 0.0
+        self.CitA_phi   = 0.0
+        self.Lip_r      = 0.0
+        self.Lip_theta  = 0.0
+        self.Lip_phi    = 0.0
 
         if filename != None:
             self.read_data(filename)
@@ -266,6 +348,7 @@ class Structure:
         Algorithm:
         Shift CitA center of mass (COM) to origin (each atom has mass 1).
         Align CitA COM - LYS34:CA (Atom 529) with positive x-axis.
+        Rotate CitA PRO71:CA (Atom 1121) around x-axis into yx-plane (positive y-axis).
         Determine spherical coordinates of Lipase COM.
         Shift Lipase COM to origin.
         Determine spherical coordinates of THR199:CA (Atom 3050).
@@ -279,6 +362,7 @@ class Structure:
 
         # Atom indices of refernece points for orientation determination
         CitArefi  = 529   # LYS34:CA
+        CitAref2i = 1121  # PRO71:CA
         LipRefi   = 3050  # THR199:CA
 
         # basis vectors
@@ -292,53 +376,101 @@ class Structure:
 
         # Compute CitA alignment rotation matrix
 
-        # CitA reference point coordinates in local CitA coordinate system
-        CitAref = self.coordinates[CitArefi*3:(CitArefi+1)*3] - CitACOM 
+        # CitA reference points coordinates in local CitA coordinate system
+        CitAref  = self.coordinates[CitArefi*3:(CitArefi+1)*3]   - CitACOM 
+        CitAref2 = self.coordinates[CitAref2i*3:(CitAref2i+1)*3] - CitACOM 
 
-        # CitA reference point angle for rotation around x-axis
-        tmpVec      = np.copy(CitAref)
+        # CitA reference point angle for rotation around y-axis into xy-plane
+        tmpVec = np.copy(CitAref)
+        tmpVec[1]   = 0.0  # project into xz-plane
+        CitAyAngle  = math.acos(np.dot(tmpVec, x) / np.linalg.norm(tmpVec)) # angle with x-axis
+
+        # Set up matrix for rotation around y-axis
+        if CitAref[2] > 0:
+            CitAyAngle = -CitAyAngle
+        Ry = np.array([[math.cos(CitAyAngle), 0, -math.sin(CitAyAngle)],
+                       [                   0, 1,                     0],
+                       [math.sin(CitAyAngle), 0,  math.cos(CitAyAngle)]]) 
+
+        # rotate around y-axis into xy-plane
+        yRotated  = np.inner(Ry, CitAref )
+        yRotated2 = np.inner(Ry, CitAref2)
+
+
+        # CitA reference point angle for rotation around z-axis onto positive x-axis
+        CitAzAngle  = math.acos(np.dot(yRotated, x) / np.linalg.norm(yRotated)) # angle with x-axis 
+
+        # Set up matrix for rotation around z-axis
+        if yRotated[1] < 0:
+            CitAzAngle = -CitAzAngle
+        Rz = np.array([[ math.cos(CitAzAngle), math.sin(CitAzAngle), 0],
+                       [-math.sin(CitAzAngle), math.cos(CitAzAngle), 0], 
+                       [                   0,                    0, 1]]) 
+
+        # Rotate around z-axis onto positive x-axis
+        zRotated  = np.inner(Rz, yRotated ) 
+        zRotated2 = np.inner(Rz, yRotated2) 
+
+
+        # CitA reference point 2 rotation around x-axis to point into positive y-direction
+        tmpVec = np.copy(zRotated2)
         tmpVec[0]   = 0.0  # project into yz-plane
-        CitAxAngle  = math.acos(np.dot(tmpVec, y) / np.linalg.norm(tmpVec)) # angle with y axis
+        CitAxAngle  = math.acos(np.dot(tmpVec, y) / np.linalg.norm(tmpVec)) # angle with y-axis 
 
         # Set up matrix for rotation around x-axis
-        if CitAref[2] < 0:
+        if zRotated2[2] < 0:
             CitAxAngle = -CitAxAngle
         Rx = np.array([[1,                     0,                    0],
                        [0,  math.cos(CitAxAngle), math.sin(CitAxAngle)],
-                       [0, -math.sin(CitAxAngle), math.cos(CitAxAngle)]])
+                       [0, -math.sin(CitAxAngle), math.cos(CitAxAngle)]]) 
 
-        # Rotate around x-axis
-        xRotated = np.inner(Rx, CitAref)
+        # Rotate CitA reference point 2 around all 3 axis
+        xRotated  = np.inner(Rx, zRotated )
+        xRotated2 = np.inner(Rx, zRotated2)
 
-        # Determine angle with positive x-axis
-        rotatedxAngle = math.acos(np.dot(xRotated, x) / np.linalg.norm(xRotated))
+        # Combine all 3 rotation matrices
+        Rxz = np.dot(Rx , Rz)
+        R   = np.dot(Rxz, Ry)
+        self.CitA_R = np.copy(R)
 
-        # Set up rotation matrix around z-axis
-        if xRotated[1] < 0:
-            rotatedxAngle = -rotatedxAngle
-        Rz = np.array([[ math.cos(rotatedxAngle), math.sin(rotatedxAngle), 0],
-                       [-math.sin(rotatedxAngle), math.cos(rotatedxAngle), 0], 
-                       [                       0,                       0, 1]])
-
-        # Rotate around z-axis
-        xzRotated = np.inner(Rz, xRotated)
+        # Rotate both reference points
+        CitArefRotated  = np.inner(R, CitAref)
+        CitAref2Rotated = np.inner(R, CitAref2) 
 
 
         # Transform Lipase COM to CitA local coordinate system
         LipCOMlocal = LipCOM - CitACOM
-        LipCOMlocal = np.inner(Rx, LipCOMlocal)
-        LipCOMlocal = np.inner(Rz, LipCOMlocal)
+        LipCOMlocal = np.inner(R, LipCOMlocal)
         
 
         # Compute sperical coordinates of Lipase COM in CitA local coordinate system
-        r     = np.linalg.norm(LipCOMlocal)
-        theta = math.acos(LipCOMlocal[2]/r)
-        phi   = math.atan2(LipCOMlocal[1], LipCOMlocal[0])
+        self.CitA_r     = np.linalg.norm(LipCOMlocal)
+        self.CitA_theta = math.acos(LipCOMlocal[2]/self.CitA_r)
+        self.CitA_phi   = math.atan2(LipCOMlocal[1], LipCOMlocal[0])
 
-        print r, theta, phi
+#        print "{:.2f}, {:.2f}, {:.2f}, {:.2f}".format(self.energy,
+#                                                      self.CitA_r,
+#                                                      self.CitA_theta/math.pi*180,
+#                                                      self.CitA_phi/math.pi*180)
 
-#        tmpVec      = CitAref / np.linalg.norm(CitAref)
-#        print CitAxAngle, tmpVec
+
+        # Transform Lipase refernece coordinates to CitA local coordinate system
+        LipRef = self.coordinates[LipRefi*3:(LipRefi+1)*3] - CitACOM 
+        LipRef = np.inner(R, LipRef)
+
+        # Center coordinates on Lipase COM
+        LipRefLocal = LipRef - LipCOMlocal
+
+        # Compute sperical coordinates of Lipase reference in lipase local coordinate system
+        self.Lip_r     = np.linalg.norm(LipRefLocal)
+        self.Lip_theta = math.acos(LipRefLocal[2]/self.Lip_r)
+        self.Lip_phi   = math.atan2(LipRefLocal[1], LipCOMlocal[0]) 
+
+#        print "{:.2f}, {:.2f}, {:.2f}, {:.2f}".format(self.energy,
+#                                                      self.Lip_r,
+#                                                      self.Lip_theta/math.pi*180,
+#                                                      self.Lip_phi/math.pi*180) 
+        
         
 
 # ==================================== #
